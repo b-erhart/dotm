0875855d1c02d2c4ec94f3fb4bc20a637f682ab9
Benedikt Erhart <contact@b-erhart.de>
2023-12-25 12:20:13 +0100

    Add base functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/b-erhart/dotm
 
 go 1.21.5
+
+require github.com/urfave/cli/v2 v2.26.0
+
+require (
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/otiai10/copy v1.14.0
+	github.com/pelletier/go-toml/v2 v2.1.1
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/u-root/u-root v0.11.0
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,167 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+	"github.com/u-root/u-root/pkg/cp"
+	"github.com/urfave/cli/v2"
+)
+
+func readConfig(configPath string) (map[string]string, error) {
+	configFile, err := os.Open(configPath)
+
+	if err != nil {
+		return nil, cli.Exit(fmt.Sprintf("Unable to read config file. Abort.\n  > %v", err), 20)
+	}
+
+	var config map[string]string
+	decoder := toml.NewDecoder(configFile)
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		var decodeErr *toml.DecodeError
+		if errors.As(err, &decodeErr) {
+			return nil, cli.Exit(fmt.Sprintf("Unable to read toml data.\n\n%v", decodeErr.String()), 20)
+		}
+
+		return nil, cli.Exit(fmt.Sprintf("Unable to read toml data.\n  > %v", err), 20)
+	}
+
+	return config, nil
+}
+
+func copy(src, dest string) bool {
+	success := true
+
+	src = os.ExpandEnv(src)
+	dest = os.ExpandEnv(dest)
+	fmt.Printf("Copying \"%s\" to \"%s\"...\n", src, dest)
+
+	opts := cp.Options{
+		NoFollowSymlinks: true,
+		PreCallback: func(_, dest string, srcfi os.FileInfo) error {
+			if _, err := os.Stat(dest); os.IsNotExist(err) {
+				return nil
+			}
+
+			fmt.Print("Destination already exists. Deleting...")
+			err := os.RemoveAll(dest)
+
+			if err != nil {
+				fmt.Printf("\nError - %v\n", err)
+				success = false
+				return cp.ErrSkip
+			}
+
+			fmt.Println(" Done.")
+
+			return nil
+		},
+	}
+
+	err := opts.CopyTree(src, dest)
+
+	if err != nil {
+		success = false
+		fmt.Printf("Error - %v\n", err)
+	} else {
+		fmt.Println("Done.")
+	}
+
+	fmt.Println()
+
+	return success
+}
+
+func distribute(ctx *cli.Context) error {
+	configPath := ctx.Args().Get(0)
+
+	if configPath == "" {
+		return cli.Exit("No config file was provided. Abort.", 10)
+	}
+
+	config, err := readConfig(configPath)
+
+	if err != nil {
+		return err
+	}
+
+	errCount := 0
+	for src, dest := range config {
+		if !copy(src, dest) {
+			errCount += 1
+		}
+	}
+
+	fmt.Printf("%d error(s) occured.", errCount)
+
+	return nil
+}
+
+func fetch(ctx *cli.Context) error {
+	configPath := ctx.Args().Get(0)
+
+	if configPath == "" {
+		return cli.Exit("No config file was provided. Abort.", 10)
+	}
+
+	config, err := readConfig(configPath)
+
+	if err != nil {
+		return err
+	}
+
+	errCount := 0
+	for src, dest := range config {
+		if !copy(dest, src) {
+			errCount += 1
+		}
+	}
+
+	fmt.Printf("%d error(s) occurred.", errCount)
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	distr := cli.Command{
+		Name:      "distribute",
+		Usage:     "copy dotfiles to specified locations",
+		ArgsUsage: "CONFIG_FILE",
+		Action:    distribute,
+		HideHelpCommand: true,
+	}
+
+	ft := cli.Command{
+		Name:      "fetch",
+		Usage:     "copy dotfiles from specified locations",
+		ArgsUsage: "CONFIG_FILE",
+		Action:    fetch,
+		HideHelpCommand: true,
+	}
+
+	app := cli.App{
+		Name:  "dotm",
+		Usage: "an incredibly simple dotfiles manager",
+		Authors: []*cli.Author{
+			{
+				Name:  "Benedikt Erhart",
+				Email: "contact@b-erhart.de",
+			},
+		},
+		Suggest:         true,
+		HideHelpCommand: true,
+		Commands: []*cli.Command{
+			&distr,
+			&ft,
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
